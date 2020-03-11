8b296c93568747e6406aec3b10083e3d115408e6
Chloe Gell <gellert.chloe@gmail.com>
2020-03-11 04:03:05 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_genres_seasons_to_shows.rb
+++ db/migrate/007_add_genres_seasons_to_shows.rb
@@ -1,6 +1,6 @@
 class AddGenresSeasonsToShows < ActiveRecord::Migration[5.2]
   def change 
     :add_column :shows, :genre, :string 
-    :add_colum  :shows, :season, :string 
+    :add_column :shows, :season, :string 
   end 
 end 
