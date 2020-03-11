214efaa51676afe43373a6838a7baebc9d4d7858
Chloe Gell <gellert.chloe@gmail.com>
2020-03-11 04:05:34 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -6,6 +6,6 @@ class Actor < ActiveRecord::Base
   end 
   
   def list_roles 
-    self.characters.map do {|c| "#{c.name} - #{c.show}"} 
+    self.characters.map {|c| "#{c.name} - #{c.show}"} 
   end 
 end
